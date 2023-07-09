217f1feb8bff4c1e59eef35e0f9b66af9440617d
Abhishek Chauhan <abhchauh6@WKMZT31E3DC6.global.publicisgroupe.net>
2023-07-09 01:53:28 +0100

    gcp commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# hybrid-cloud
+1.Create Resources
+  a. VM machines
+  b. sql database
+  c. vpc,firewalls
+  d. web-server
+  e. load balancer, auto scaler
+
+2.  
---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "google" {
+  credentials = var.google_credentials
+  project = "learning-384823"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+variable "google_credentials" {
+  description = "Google Cloud service account credentials"
+  type        = string
+}
